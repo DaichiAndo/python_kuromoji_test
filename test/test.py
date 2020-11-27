59362c710ee7848586471eda1d951a070708358d
安藤 大智 <d-ando@PC877.local>
2020-11-27 13:54:58 +0900

    ユーザー定義例外に引数を追加

--- test/test.py
+++ test/test.py
@@ -18,8 +18,8 @@ def has_duplicates(seq):
 
 
 # ユーザー定義例外
-class Error(Exception):
-    print('aaaaa')
+class Error(Exception, number_of_lines):
+    print('aaaaa' + number_of_lines)
 
 
 # ファイルを一行ずつ読み込み
@@ -41,7 +41,7 @@ for line in lines:
     elements[2] = elements[2].lstrip('"')
 
     if len(elements) != 4:
-        raise Error('要素数が不適切です')
+        raise Error('要素数が不適切です', number_of_lines)
 
     # ４つ目以外の各要素内のダブルクォートの数を確認（ブランド名にダブルクォートが含まれていないか）：チェックポイント①-2
     if elements[0].count('"') != 0 or elements[1].count('"') != 0 or elements[2].count('"') != 0:
